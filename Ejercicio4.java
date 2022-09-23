0b1089250d323459dd3a54341d02dcec8982e70b
Isaias Salinas <isalorsaw@hotmail.com>
2022-09-22 18:39:21 -0600

    Intercambio inicial de proyecto

---
+++ Ejercicio4.java
@@ -0,0 +1,28 @@
+/*
+   Programa que pide un numero y muestra si es mayor,menor o igual que 15
+*/
+import javax.swing.JOptionPane;
+public class Ejercicio4
+{//Inicio
+    public static void main(String args[])
+    {//Inicio
+        JOptionPane.showMessageDialog(null,"Este programa pedira un numero y mostrara si es menor, mayor o igual a 15");
+        
+        String entrada=JOptionPane.showInputDialog("Ingrese un Numero");
+        int num=Integer.parseInt(entrada);
+        
+        if(num<15)
+        {
+            JOptionPane.showMessageDialog(null,"El Numero que usd ingreso es menor a 15");
+        }
+        else if(num>15)
+        {
+            JOptionPane.showMessageDialog(null,"El Numero que usd ingreso es mayor a 15");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null,"El Numero que usd ingreso es igual a 15");
+        }
+        
+    }//Fin
+}//Fin
---
+++ Ejercicio5.java
@@ -0,0 +1,15 @@
+/*
+   Programa que pide un nombre y un apellido y los muestra concatenados en pantalla
+   */
+  public class Ejercicio5
+  {
+      public static void main(String args[])
+        {
+            javax.swing.JOptionPane.showMessageDialog(null,"Este programa pedira un nombre y un apellido y lo mostrara concatenado en pantalla");
+            
+            String nombre=javax.swing.JOptionPane.showInputDialog("Ingrese un Nombre");
+            String apellido=javax.swing.JOptionPane.showInputDialog("Ingrese un Apellido");
+            String concat=nombre+" "+apellido;
+            javax.swing.JOptionPane.showMessageDialog(null,concat);
+        }
+  }

478d2e343607ebb884ef01b123913fb0e9fd16d6
isalorsaw <isalorsaw@hotmail.com>
2022-09-27 18:09:15 -0600

    kdj

--- Ejercicio12.java
+++ Ejercicio12.java
@@ -11,25 +11,25 @@
           //Declarar e Inicializar Variables
           int totalpagar=0;
           
-          String m=JOptionPane.showInputDialog("Menu\n1. Sol\n2. Sombra");
-          int menu=Integer.parseInt(m);
+          String m=JOptionPane.showInputDialog("Menu\n1. Sol\n2. Sombra");//Entrada=Input
+          int menu=Integer.parseInt(m);//Convertir de String a Int
           int cantidad=0;
           
           if(menu==1)
           {
-                m=JOptionPane.showInputDialog("Cuantos Boletos desea");
-              cantidad=Integer.parseInt(m);
+                m=JOptionPane.showInputDialog("Cuantos Boletos desea");//Entrada=Input
+              cantidad=Integer.parseInt(m);//Convertir de String a Int
               totalpagar=cantidad*150;
-              JOptionPane.showMessageDialog(null,"Total a Pagar sera: "+totalpagar);
+              JOptionPane.showMessageDialog(null,"Total a Pagar sera: "+totalpagar);//Salida MessageDialog
           }
           else if(menu==2)
           {
-               m=JOptionPane.showInputDialog("Cuantos Boletos desea");
-              cantidad=Integer.parseInt(m);
+               m=JOptionPane.showInputDialog("Cuantos Boletos desea");//Entrada=Input
+              cantidad=Integer.parseInt(m);//Convertir de String a Int
               totalpagar=cantidad*400;
-              JOptionPane.showMessageDialog(null,"Total a Pagar sera: "+totalpagar);
+              JOptionPane.showMessageDialog(null,"Total a Pagar sera: "+totalpagar);//Salida MessageDialog
           }
-          else JOptionPane.showMessageDialog(null,"Opcion Invalida");
+          else JOptionPane.showMessageDialog(null,"Opcion Invalida");//Salida MessageDialog
           
       }
   }

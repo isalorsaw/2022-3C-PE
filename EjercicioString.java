f84550c00aa05a625f8cd0d9ef8a5dd0238a9758
isalorsaw <isalorsaw@hotmail.com>
2022-09-28 18:31:15 -0600

    Inicio de Cadenas

---
+++ EjercicioString.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class EjercicioString
+{
+    public static void main(String args[])
+    {
+        String cadena=JOptionPane.showInputDialog("Ingrese una Cadena");
+        String menu=JOptionPane.showInputDialog("Menu.\n1. Mostrar tamaño \n2. Mostrar la Ultima Letra\n3. Mostrar la Primera Letra\n4 Convertir a Mayusculas\n5 Convertir a Minusculas");
+    }
+}

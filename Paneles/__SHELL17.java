6e1acc2b19a71fcbbbf837f77b7e8cebdc915730
isalorsaw <isalorsaw@hotmail.com>
2022-10-19 18:28:53 -0600

    Inicio de Ciclos en un For 19/10

--- Paneles/__SHELL17.java
+++
@@ -1,8 +0,0 @@
-package Paneles;
-public class __SHELL17 extends bluej.runtime.Shell {
-public static void run() throws Throwable {
-
-java.lang.String[] __bluej_param0 = { };
-Paneles.Ejemplo2.main(__bluej_param0);
-
-}}
---
+++ parcial2/EjemploCiclo.java
@@ -0,0 +1,56 @@
+package parcial2;
+/*
+   Pedir 5 numeros y mostrar cuantos son pares.
+   */
+import javax.swing.JOptionPane;
+public class EjemploCiclo
+{
+    public static void main(String args[])
+    {
+        //Ciclo For
+        int n=0;
+        int contar=0;//Contador
+        int suma=0;//Acumulador
+        String pares="";
+        
+        int contari=0;
+        int sumai=0;
+        String impares="";
+        
+        for(int i=0;i<5;i++)
+        {
+            try
+            {
+                n=Integer.parseInt(JOptionPane.showInputDialog("Ingrese un Numero"));
+                
+                if(n%2==0)
+                {
+                    contar++;//contar=contar+1;
+                    suma=suma+n;//suma+=n;
+                    pares=pares+" "+n;
+                }
+                else
+                {
+                    contari++;
+                    sumai=sumai+n;
+                    impares+=n+" ";
+                }
+            }
+            catch(Exception exp)
+            {
+                JOptionPane.showMessageDialog(null,exp);
+            }
+        }
+        JOptionPane.showMessageDialog(null,"Ingrese "+contar+" numeros Pares\nLa suma de ellos es "+
+        suma+"\nLos numeros pares fueron "+pares);
+        
+        JOptionPane.showMessageDialog(null,"Ingrese "+contari+" numeros ImPares\nLa suma de ellos es "+
+        sumai+"\nLos numeros impares fueron "+impares);
+        /*int i=0;
+        while(i<5)
+        {
+            i++;
+        }*/
+        
+    }
+}

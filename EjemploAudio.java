f14aa5ca7500a34dd6ee428bf67c4948ccba96ca
isalorsaw <isalorsaw@hotmail.com>
2022-11-21 18:31:33 -0600

    Ejemplo de Audio 21/11

---
+++ EjemploAudio.java
@@ -0,0 +1,20 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.util.Scanner;
+public class EjemploAudio 
+{
+    public static void main(String[] args) 
+    {
+        String filename = "mariobros.MP3";//Inicializamos la Ruta
+        MP3Player mp3Player = new MP3Player(filename);//Inicializamos MP3Player
+        mp3Player.play();//Reproducimos
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write stop to stop the music: ");
+
+        if (sc.nextLine().equalsIgnoreCase("stop")) {
+            mp3Player.close();
+        }
+
+    }
+}

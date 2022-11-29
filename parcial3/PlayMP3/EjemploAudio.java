527a27cf13a455240251b4f3430b56ed95aaeb79
isalorsaw <isalorsaw@hotmail.com>
2022-11-29 17:48:50 -0600

    Actualizacion de parcial3.PlayMP3 para reproduccion de audio 29/11

--- parcial3/PlayMP3/EjemploAudio.java
+++ parcial3/PlayMP3/EjemploAudio.java
@@ -2,11 +2,19 @@ package parcial3.PlayMP3;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.util.Scanner;
+import java.io.File;
 public class EjemploAudio 
 {
     public static void main(String[] args) 
     {
-        String filename = "mariobros.mp3";//Inicializamos la Ruta
+       /* String filename = "mariobros.mp3";//Inicializamos la Ruta
+        MP3Player mp3Player = new MP3Player(filename);//Inicializamos MP3Player
+        mp3Player.play();//Reproducimos*/
+        
+        File file=new File("parcial3/PlayMp3/mp3/mariobros.mp3");
+        String filename=file.getAbsolutePath();
+        System.out.println(filename);
+        
         MP3Player mp3Player = new MP3Player(filename);//Inicializamos MP3Player
         mp3Player.play();//Reproducimos
 
